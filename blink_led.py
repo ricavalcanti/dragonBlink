4bb762d3fa19d3e45a242c5b4478cd0d81ce6a5e
ricardo <ricavalcanti@bct.ect.ufrn.br>
2018-10-21 17:58:02 -0300

    First test with ultrassonicSensor

--- blink_led.py
+++ blink_led.py
@@ -5,24 +5,34 @@ GP = GPIOProcessor()
 
 try:
     trig = GP.getPin29() 
-    Pin27.out()
+    trig.out()
 
     echo = GP.getPin30() 
-    Pin29.input()	
+    echo.input()	
 
     trig.low()
     print("Wait for sensor to settle")
     time.sleep(2)
-    trig.high()
-    time.sleep(0.00001)
-    trig.low()
-
-    while(echo.getValue == 0):
-        pulse_start = time.clock()
-    while(echo.getValue() == 1):
-        pulse_end = time.clock()
-    pulse_duration = pulse_end - pulse_start
-    print("Duration: " + str(pulse_duration))
+    
+    pulseStart = 0
+    pulseEnd = 0	
+   # while(echo.getValue == 0):
+    #    pulse_start = time.clock()
+   # while(echo.getValue() == 1):
+    #    pulse_end = time.clock()
+   # pulse_duration = pulse_end - pulse_start
+    while(1):
+	trig.high()
+	time.sleep(0.00001)
+	trig.low()
+	while(echo.getValue() == 0):
+		pulseStart = time.time()
+    	while(echo.getValue() == 1):
+		pulseEnd = time.time()
+	distance = (pulseEnd - pulseStart)*17150
+	distance = round(distance,2)
+	print("Distance: "+ str(distance))
+	time.sleep(1)
 
 finally:
     GP.cleanup()
