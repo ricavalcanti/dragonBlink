668cb99e2aee593e29c371eea018de5ebf609f54
ricavalcanti <ricavalcanti@bct.ect.ufrn.br>
2018-10-27 17:59:49 -0300

    array of values

--- usingUltrassonic.py
+++ usingUltrassonic.py
@@ -13,29 +13,33 @@ try:
     trig.low()
     print("Wait for sensor to settle")
     time.sleep(2)
-    while(1):
-	pulseStart = 0
-	pulseEnd = 0
-    	isOutR = False
-    	init = time.clock()	
-	trig.high()
-	time.sleep(0.00001)
-	trig.low()
-	while(echo.getValue() == 0):
-		pulseStart = time.clock()
-		if(pulseStart - init > 0.3):
-			print("out of range")
-            		isOutR = True
-			break
-	pulseEnd = time.clock()
-    	while(echo.getValue() == 1):
-		pulseEnd = time.clock()
-    	if(not isOutR):
-        	distance = (pulseEnd - pulseStart)*17150
-	    	distance = round(distance,2)
-		if(distance < 20):
-	    		print("Distance: "+ str(distance))
-	time.sleep(2)
-
+    for i in range(15):
+        pulseStart = 0
+        pulseEnd = 0
+            isOutR = False
+            init = time.clock()	
+        trig.high()
+        time.sleep(0.00001)
+        trig.low()
+        while(echo.getValue() == 0):
+            pulseStart = time.clock()
+            if(pulseStart - init > 0.3):
+                print("out of range")
+                        isOutR = True
+                break
+        pulseEnd = time.clock()
+            while(echo.getValue() == 1):
+            pulseEnd = time.clock()
+            if(not isOutR):
+                distance = (pulseEnd - pulseStart)*17150
+                distance = round(distance,2)
+            if(distance < 20 and distance >3) :
+                    print("Distance: "+ str(distance))
+                    distances.append(distance)
+            else:
+                i = i-1
+        time.sleep(2)
+    print("Terminado")
+    
 finally:
     GP.cleanup()
